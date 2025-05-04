02d49af9ecf649c31bc263f95f4d74d3583320c7
marleychanson <michaelboyack89@gmail.com>
2025-05-04 12:23:31 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Welcome to SunnyPint</h1>
+      <p>Find beer gardens in the UK that are currently in the sun.</p>
+    </div>
+  );
+}
+
+export default App;
